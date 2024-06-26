1428b76ac5011b5ab3dfbe3eb380ba42a276cd38
malith94batugedara <malithbatugedara@gmail.com>
2024-06-26 22:12:31 +0530

    controllers and views creation done

---
+++ app/Http/Controllers/GalleryController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gallery;
+use App\Http\Requests\StoreGalleryRequest;
+use App\Http\Requests\UpdateGalleryRequest;
+use Illuminate\View\View;
+
+class GalleryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index():View
+    {
+        return view('galleries.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreGalleryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Gallery $gallery)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Gallery $gallery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateGalleryRequest $request, Gallery $gallery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Gallery $gallery)
+    {
+        //
+    }
+}
